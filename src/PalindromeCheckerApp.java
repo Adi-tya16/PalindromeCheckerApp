03e92075784405392f9a7a61a9803564462b2453
Adi-tya16 <sharma77aditya@gmail.com>
2026-03-02 14:39:09 +0530

    UC2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,10 +1,27 @@
+
 public class PalindromeCheckerApp {
 
-        public static void main(String[] args) {
+    /**
+     * Application entry point for UC2.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
 
-            System.out.println("Welcome to the Palindrome Checker Management System");
-            System.out.println("Version : 1.0");
-            System.out.println("System initialized successfully.");
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        int inputLength = input.length();
+
+        // Loop only till half of the string length
+        for (int i = 0; i < inputLength / 2; i++) {
+            if (input.charAt(i) != input.charAt(inputLength - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
         }
-    }
 
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
+}
